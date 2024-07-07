42c4fafc4a875630329421350d9edcf6662479e4
Jose Suarez <jsuarez@hangarcentral.com>
2024-07-07 18:40:49 -0400

    sw

---
+++ public/service-worker.js
@@ -0,0 +1,52 @@
+const installEvent = () => {
+    self.addEventListener('install', () => {
+      console.log('service worker installed');
+    });
+  };
+  installEvent();
+  
+  const activateEvent = () => {
+    self.addEventListener('activate', () => {
+      console.log('service worker activated');
+    });
+  };
+  activateEvent();
+
+ 
+
+
+  const installPromp = () => {
+    console.log('antes beforeinstallprompt');
+    self.addEventListener('beforeinstallprompt', e => {
+      e.prompt();
+      console.log('beforeinstallprompt');
+    });
+  };
+  installPromp();
+  
+  
+  
+  const cacheName = 'v2';
+
+
+  
+  const cacheClone = async (e) => {
+    const res = await fetch(e.request);
+    const resClone = res.clone();
+  
+    const cache = await caches.open(cacheName);
+    await cache.put(e.request, resClone);
+    return res;
+  };
+  
+  const fetchEvent = () => {
+    self.addEventListener('fetch', (e) => {
+      e.respondWith(
+        cacheClone(e)
+          .catch(() => caches.match(e.request))
+          .then((res) => res)
+      );
+    });
+  };
+  
+  fetchEvent();
